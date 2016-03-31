24f8f48b4e59269fb6bc153dc4e48f7f6b7b8c17
EddieWilkie <edward.wilkie.2013@uni.strath.ac.uk>
2016-03-31 21:35:38 +0100

    Created the listener Socket which listens for httpRequests.

--- ACE4/src/WebServer.java
+++ ACE4/src/WebServer.java
@@ -1,13 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 public final class WebServer {
 	
-	public static void main(String[] args){
-
+	public static void main(String[] args) throws IOException{
+		//set port number
+		final int port = 6789;
+		@SuppressWarnings("resource")
+		ServerSocket socket = new ServerSocket(port);
+		while(true){
+			Socket client = socket.accept();
+			HttpRequest request = new HttpRequest(client);
+			Thread thread = new Thread(request);
+			thread.start();
+		}
 	}
 
 }
 
-final class HTTPRequest implements Runnable{
+final class HttpRequest implements Runnable{
+	Socket client;
+	public HttpRequest(Socket client){
+		this.client = client;
+	}
 
 	@Override
 	public void run() {
